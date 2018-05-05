e62f53ac4ce7c832b653b44ea064f0d302c2ee5c
Can PERK <bayms8416@gmail.com>
2018-05-05 14:03:02 +0300

    YMS Bank initialize

---
+++ BilgeAdam.YMSBank/BilgeAdam.YMSBank.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BilgeAdam.YMSBank.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ BilgeAdam.YMSBank/BilgeAdam.YMSBank.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BilgeAdam.YMSBank.Data
+{
+    public class Class1
+    {
+    }
+}
