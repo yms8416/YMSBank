0c47fe1a5cb8a672522d95a3d923558225910b80
Can PERK <bayms8416@gmail.com>
2018-05-12 12:35:32 +0300

    entities on context

--- BilgeAdam.YMSBank/BilgeAdam.YMSBank.Data/Context/YMSContext.cs
+++ BilgeAdam.YMSBank/BilgeAdam.YMSBank.Data/Context/YMSContext.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BilgeAdam.YMSBank.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BilgeAdam.YMSBank.Data.Context
 {
-    public class YMSContext
+    public class YMSContext : DbContext
     {
+        public YMSContext(DbContextOptions<YMSContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<AccountHistory> AccountHistories { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<GenericLookup> GenericLookups { get; set; }
+        public virtual DbSet<GenericLookupType> GenericLookupTypes { get; set; }
+        public virtual DbSet<Menu> Menus { get; set; }
+        public virtual DbSet<Person> People { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<Transfer> Transfers { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserRole> UserRoles { get; set; }
     }
 }
