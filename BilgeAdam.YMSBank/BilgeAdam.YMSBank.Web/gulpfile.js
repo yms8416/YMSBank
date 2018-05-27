1d4192f9ab0c5c40327b834e6b814b056b3ca4c1
Can PERK <bayms8416@gmail.com>
2018-05-27 14:16:11 +0300

    api için ödev

--- BilgeAdam.YMSBank/BilgeAdam.YMSBank.Web/gulpfile.js
+++ BilgeAdam.YMSBank/BilgeAdam.YMSBank.Web/gulpfile.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
  
-var paths = {
-  css: ['wwwroot/css/**/*.css']
-};
- 
-gulp.task('clean', function() {
-  return del(['build']);
+gulp.task('cleanJs', function () {
+    return del(['wwwroot/build/js/all.min.js']
+    );
+});
+
+gulp.task('cleanCss', function () {
+    return del(['wwwroot/build/css/all.min.css']
+    );
 });
  
-gulp.task('scripts', ['clean'], function () {
+gulp.task('scripts', ['cleanJs'], function () {
     var files = [
         "wwwroot/js/jquery.min.js",
         "wwwroot/js/bootstrap.min.js",
@@ -25,6 +26,19 @@ gulp.task('scripts', ['clean'], function () {
       .pipe(uglify())
       .pipe(concat('all.min.js'))
     .pipe(gulp.dest('wwwroot/build/js'));
+});
+
+gulp.task('styles', ['cleanCss'], function () {
+    var files = [
+        "wwwroot/css/bootstrap.min.css",
+        "wwwroot/css/font-awesome.min.css",
+        "wwwroot/css/nprogress.css",
+        "wwwroot/css/custom.min.css",
+        "wwwroot/css/site.css"
+    ];
+    return gulp.src(files)
+        .pipe(concat('all.min.css'))
+        .pipe(gulp.dest('wwwroot/build/css'));
 });
  
 gulp.task('watch', function() {
