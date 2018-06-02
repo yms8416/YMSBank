24c29115c2812f78e7d20ac83088ef47ce590d4d
Can PERK <bayms8416@gmail.com>
2018-06-02 12:06:38 +0300

    View Component lar layout üzerinde kullanıldı. html daha anlaşılır hale getirildi

--- BilgeAdam.YMSBank/BilgeAdam.YMSBank.Web/Startup.cs
+++ BilgeAdam.YMSBank/BilgeAdam.YMSBank.Web/Startup.cs
@@ -42,6 +42,11 @@ namespace BilgeAdam.YMSBank.Web
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
